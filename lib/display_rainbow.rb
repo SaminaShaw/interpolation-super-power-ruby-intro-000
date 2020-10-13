6a48508f45d1cacb779d4b222edf03be5d0a8af8
hilarious-recipe-9721 <saminashaw3@gmail.com>
2020-10-13 17:16:18 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,3 @@
-def display_rainbow("R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: vi
-olet)
-  puts "the color red #{red[0]}"
-  puts "#{orange[1]}"
-  puts "#{yellow[2]}"
-  puts "#{green[3]}"
-  puts "#{blue[4]}"
-  puts "#{indigo[5]}"
-  puts "#{violet[6]}"
-end
+def display_rainbow
+  end
  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
